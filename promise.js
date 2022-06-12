5868a1da02fe5a0aefbf6d9b780e5127f5e0a164
pulau <muchammad.anas@gmail.com>
2022-06-12 19:31:26 +0700

    1 - JS Promise - Tantangan

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const countEmosiIXX = theaterIXX.filter((item) => item.hasil === emosi).length;
+  const countEmosiVGC = theaterVGC.filter((item) => item.hasil === emosi).length;
+  const total = countEmosiIXX + countEmosiVGC;
+  // return `${emosi} ${total}`;
+  return `${total}`;
+}
 
 module.exports = {
   promiseOutput,
